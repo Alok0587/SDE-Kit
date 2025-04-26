8b6f13411a232464a7d12d4eab84ceaae68e35b3
Alok <alok.kumar@bhn.com>
2025-04-27 01:40:59 +0530

    Pushing to git

---
+++ src/main/java/com/example/sdekit/SdeKitApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdeKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdeKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdekit/SdeKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdeKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
